ffaad76f3fb5283cd64bea016d098c59dbf02558
ikadgzl <ikadgzl@gmail.com>
2023-05-01 13:33:36 +0300

    add: order service

---
+++ service/order.go
@@ -0,0 +1,84 @@
+package service
+
+import (
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/ikadgzl/ddd-in-go/aggregate"
+	"github.com/ikadgzl/ddd-in-go/domain/customer"
+	cm "github.com/ikadgzl/ddd-in-go/domain/customer/memory"
+	"github.com/ikadgzl/ddd-in-go/domain/product"
+	pm "github.com/ikadgzl/ddd-in-go/domain/product/memory"
+)
+
+type OrderConfig func(os *OrderService) error
+
+type OrderService struct {
+	cr customer.CustomerRepository
+	pr product.ProductRepository
+}
+
+func NewOrderService(cfgs ...OrderConfig) (*OrderService, error) {
+	os := &OrderService{}
+
+	for _, cfg := range cfgs {
+		err := cfg(os)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return os, nil
+}
+
+func WithCustomerRepository(cr customer.CustomerRepository) OrderConfig {
+	return func(os *OrderService) error {
+		os.cr = cr
+		return nil
+	}
+}
+
+func WithMemoryCustomerRepository() OrderConfig {
+	cr := cm.New()
+
+	return WithCustomerRepository(cr)
+}
+
+func WithMemoryProductRepository(products []aggregate.Product) OrderConfig {
+	pr := pm.New()
+
+	return func(os *OrderService) error {
+		for _, p := range products {
+			err := pr.Add(p)
+			if err != nil {
+				return err
+			}
+		}
+
+		os.pr = pr
+		return nil
+	}
+}
+
+func (o *OrderService) CreateOrder(customerID uuid.UUID, productIDs []uuid.UUID) error {
+	c, err := o.cr.Get(customerID)
+	if err != nil {
+		return err
+	}
+
+	var products []aggregate.Product
+	var total float64
+
+	for _, id := range productIDs {
+		p, err := o.pr.GetByID(id)
+		if err != nil {
+			return err
+		}
+
+		products = append(products, p)
+		total += p.GetPrice()
+	}
+
+	log.Printf("Customer: {%s} has ordered %d products with the total price of %f", c.GetID(), len(products), total)
+	return nil
+}
---
+++ service/order_test.go
@@ -0,0 +1,63 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/ikadgzl/ddd-in-go/aggregate"
+)
+
+func init_products(t *testing.T) []aggregate.Product {
+	beer, err := aggregate.NewProduct("Beer", "Nice beer", 8.99)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	burger, err := aggregate.NewProduct("Burger", "Fills you up", 14.99)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chips, err := aggregate.NewProduct("Chips", "Very crispy", 4.99)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return []aggregate.Product{
+		beer, burger, chips,
+	}
+}
+
+func TestOrder_NewOrderService(t *testing.T) {
+	products := init_products(t)
+
+	os, err := NewOrderService(
+		WithMemoryCustomerRepository(),
+		WithMemoryProductRepository(products),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c, err := aggregate.NewCustomer("Ilker")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.cr.Add(c)
+	if err != nil {
+		t.Error(err)
+	}
+
+	orders := []uuid.UUID{
+		products[0].GetID(),
+		products[1].GetID(),
+		products[2].GetID(),
+	}
+
+	err = os.CreateOrder(c.GetID(), orders)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
